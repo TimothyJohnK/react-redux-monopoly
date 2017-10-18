9fab605aba499e17677eb4f0d3e1ec9588f040dc
David Frenk <David.Frenk@irco.com>
2017-10-18 14:17:23 -0500

    checking in latest before switch to gitlab

--- src/common/helpers/squareMap.test.js
+++ src/common/helpers/squareMap.test.js
@@ -4,7 +4,11 @@ for (let i = 0; i < 11; i++) {
   let line = ''
   for (let j = 0; j < 11; j++) {
     const p = 10 * i + j
-    line += ` | ${squareMap[p].name} | `
+    const cell = squareMap[p]
+    if (cell.name) {
+      name = (' ' + cell.name + '        ').substring(0, 11)
+    }
+    line += ` | ${name} | `
   }
   console.log(line)
 }
