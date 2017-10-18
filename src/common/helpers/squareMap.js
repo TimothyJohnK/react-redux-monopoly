c5b72b8375461d923adb4d34299ce3814de93f27
David Frenk <David.Frenk@irco.com>
2017-10-18 13:37:04 -0500

    fixed board image

---
+++ src/common/helpers/squareMap.js
@@ -0,0 +1,90 @@
+module.exports = [
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'pall mall',
+    color: 'purple',
+    price: 5302,
+  },
+  {
+    board: true,
+    name: 'electric company',
+    price: 55
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: true,
+    name: 'jail',
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: false,
+  },
+  {
+    board: true,
+    name: 'strand',
+    price: 3732
+
+  },
+]
