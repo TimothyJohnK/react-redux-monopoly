4769391e04dfe38268ad726e9acd81e55b150e1c
Steven Olson <steve.olson@trane.com>
2017-10-18 14:26:47 -0500

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
+// Testing
+
 import initStore from 'store'
 import App from 'components/app.jsx'
 
